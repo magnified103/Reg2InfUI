1566feced71d0710c00c447a61666ade636bffee
Nguyen Viet Dung <dung11112003@gmail.com>
2018-07-25 23:55:27 +0700

    Add files via upload

---
+++ Reg2InfUI/Reg2InfUI/RegistryEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Reg2InfUI
+{
+    class RegistryEnum
+    {
+        RegistryValueKind KeyType;
+        object KeyValue;
+    }
+}
---
+++ Reg2InfUI/Reg2InfUI/RegistryType.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Reg2InfUI
+{
+
+    class RegistryType
+    {
+        RegistryKey regKey = Registry.LocalMachine;
+
+        protected RegistryKey openKey(string location)
+        {
+            regKey = regKey.OpenSubKey(location);
+            return regKey;
+        }
+
+        protected List<string> listSubKey()
+        {
+            List<string> lsSubKey = new List<string>();
+            if (regKey != null)
+            {
+                string[] temp = regKey.GetSubKeyNames();
+                foreach (string str in temp)
+                {
+                    lsSubKey.Add(str);
+                }
+                return lsSubKey;
+            }
+            return lsSubKey;
+        }
+        protected bool Is_SubKey_Exist(string location)
+        {
+            regKey = regKey.OpenSubKey(location);
+            if (regKey != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        protected bool Is_KeyName_Exist(string KeyName, string location)
+        {
+            regKey = regKey.OpenSubKey(location);
+            if (Registry.GetValue(location, KeyName, null) != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
