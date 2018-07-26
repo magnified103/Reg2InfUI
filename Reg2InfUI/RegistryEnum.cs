23dc0d9c3a4348d96de6371d26f370ea26616bbf
Nguyen Viet Dung <dung11112003@gmail.com>
2018-07-26 14:59:47 +0700

    Re-added files+add more stuff

---
+++ Reg2InfUI/RegistryEnum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace Reg2InfUI
+{
+    class RegistryEnum
+    {
+        string KeyName;
+        RegistryValueKind KeyType;
+        object KeyValue;
+
+        protected string bitmask_value()
+        {
+            switch (KeyType)
+            {
+                case RegistryValueKind.Binary:
+                    return "0x00000001";
+                case RegistryValueKind.DWord:
+                    return "0x00010001";
+                case RegistryValueKind.ExpandString:
+                    return "0x00020000";
+                case RegistryValueKind.MultiString:
+                    return "0x00010000";
+                case RegistryValueKind.None:
+                    return "0x00020001";
+                case RegistryValueKind.String:
+                    return "0x00000000";
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
